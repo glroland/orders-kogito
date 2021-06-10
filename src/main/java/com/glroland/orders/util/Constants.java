eb2313285b09243c77976af973596189fcb67c69
Lee Roland <glroland@hotmail.com>
2021-06-10 15:16:52 -0400

    basic functionality, keeping github up to date with incremental changes

---
+++ src/main/java/com/glroland/orders/util/Constants.java
@@ -0,0 +1,13 @@
+package com.glroland.orders.util;
+
+public class Constants 
+{
+    public static class IncomingOrderStatus
+    {
+        public static final String NEW = "NEW";
+        public static final String INVALID = "INVALID";
+        public static final String FRAUD = "FRAUD";
+        public static final String READY = "READY";
+
+    }
+}
